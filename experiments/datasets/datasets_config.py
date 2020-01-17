617ed2f2a35f89a12f71b581e4eff5a2959529cb
geolil <44433775+geolil@users.noreply.github.com>
2020-01-17 18:50:12 +0100

    Create datasets_config.py

---
+++ experiments/datasets/datasets_config.py
@@ -0,0 +1,70 @@
+import time
+import os
+import random
+
+from models.gunet_diffpool1 import diff_pool_net1
+from models.gunet_diffpool2 import diff_pool_net2
+from models.gunet_topk import gunet_topk
+from models.gunet_sagpool import sagpool
+
+config = {}
+
+
+pool_ratios = [0.35, 0.35]
+
+# model parameters
+config['model'] = {}
+
+# diffpool 1
+config['model']['diff_pool_net1'] = {}
+config['model']['diff_pool_net1']['model_constructor'] = diff_pool_net1
+config['model']['diff_pool_net1']['parameters'] = {}
+config['model']['diff_pool_net1']['parameters']['num_clusters1'] = int(2708*pool_ratios[0])
+config['model']['diff_pool_net1']['parameters']['num_clusters2'] = int(2708*pool_ratios[0]*pool_ratios[1])
+config['model']['diff_pool_net1']['parameters']['hidden_channels'] = 32
+
+# diffpool 2
+config['model']['diff_pool_net2'] = {}
+config['model']['diff_pool_net2']['parameters'] = {}
+config['model']['diff_pool_net2']['model_constructor'] = diff_pool_net2
+config['model']['diff_pool_net2']['parameters']['num_clusters1'] = int(2708*pool_ratios[0])
+config['model']['diff_pool_net2']['parameters']['num_clusters2'] = int(2708*pool_ratios[0]*pool_ratios[1])
+config['model']['diff_pool_net2']['parameters']['n_hidden'] = 64
+config['model']['diff_pool_net2']['parameters']['hidden_channels'] = 32
+
+
+# topk
+config['model']['topk'] = {}
+config['model']['topk']['parameters'] = {}
+config['model']['topk']['model_constructor'] = gunet_topk
+config['model']['topk']['parameters']['depth'] = 2
+config['model']['topk']['parameters']['pool_ratios'] = pool_ratios
+config['model']['topk']['parameters']['dropout_rate'] = 0.
+config['model']['topk']['parameters']['hidden_channels'] = 32
+
+# sagpool
+config['model']['sagpool'] = {}
+config['model']['sagpool']['parameters'] = {}
+config['model']['sagpool']['model_constructor'] = gunet_topk
+config['model']['sagpool']['parameters']['depth'] = 2
+config['model']['sagpool']['parameters']['pool_ratios'] = pool_ratios
+config['model']['sagpool']['parameters']['dropout_rate'] = 0.
+config['model']['sagpool']['parameters']['hidden_channels'] = 32
+
+# loss
+config['loss_lambda'] = 0
+
+# optimizer
+config['optimizer'] = {}
+config['optimizer']['lr'] = 0.01
+
+
+
+# epochs and repeats
+config['epochs'] = 100
+config['tracked_epochs'] = [1, 10, 25, 50, 100, 150]
+config['n_repeats'] = 2
+config['patience'] = 100
+
+# output
+config['output_file'] = os.path.join('experiments', 'datasets', 'datasets_results.pkl')
