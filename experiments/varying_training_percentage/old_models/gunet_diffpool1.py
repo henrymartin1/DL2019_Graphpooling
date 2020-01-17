5821e95af6d679a4215b3464ce4d3144f4bcd2a4
christianbohn <cbohn@student.ethz.ch>
2020-01-17 20:32:53 +0100

    var train percentage experiments include updated models now

--- experiments/varying_training_percentage/old_models/gunet_diffpool1.py
+++ experiments/varying_training_percentage/old_models/gunet_diffpool1.py
@@ -14,7 +14,6 @@ from torch_geometric.nn import dense_diff_pool
 from torch_geometric.nn import GCNConv
 from torch_geometric.utils import to_scipy_sparse_matrix, to_dense_adj, \
     dense_to_sparse, from_scipy_sparse_matrix, from_networkx
-from torch_geometric.utils import dropout_adj
 
 sys.path.append(os.path.join(os.getcwd(), '..'))
 
@@ -32,13 +31,8 @@ class GCN(torch.nn.Module):
 
 
 class diff_pool_net1(torch.nn.Module):
-    def __init__(self, dataset, num_clusters1, num_clusters2, n_hidden=64, hidden_channels=None, \
-                    p_dropout_adj=0.2, p_dropout_nodes=0.92):
-        super(diff_pool_net1, self).__init__()
-
-        # initial dropout
-        self.initial_dropout_adj = p_dropout_adj
-        self.initial_dropout_nodes = p_dropout_nodes
+    def __init__(self, dataset, num_clusters1, num_clusters2, n_hidden=64, hidden_channels=None):
+        super(diff_pool_net1, self).__init__()      
             
         # level 0: original graph
         self.gcn0_in = GCN(n_in=dataset.num_features, n_out=hidden_channels, n_hid=n_hidden)
@@ -56,19 +50,13 @@ class diff_pool_net1(torch.nn.Module):
 
     def forward(self, data, mask):
         # unpack the data container
-        #  x0, edge_index0, edge_weight0 = data.x, data.edge_index, data.edge_attr
-
-
-        edge_index0, _ = dropout_adj(
-            data.edge_index, p=self.initial_dropout_adj, force_undirected=True,
-            num_nodes=data.num_nodes, training=self.training)
-        x0 = F.dropout(data.x, p=self.initial_dropout_nodes, training=self.training)
+        x0, edge_index0, edge_weight0 = data.x, data.edge_index, data.edge_attr
 
         # level 0 conv  
-        x0_ = self.gcn0_in(x0, edge_index0)
+        x0_ = self.gcn0_in(x0, edge_index0, edge_weight0)
 
         # pooled 1 
-        s1 = F.relu(self.conv_pool1(x0_, edge_index0))
+        s1 = F.relu(self.conv_pool1(x0_, edge_index0, edge_weight0))
         x1, adj1, l1, e1 = dense_diff_pool(x0_, data.adj, s1, mask)
         x1 = torch.squeeze(x1)
         
@@ -100,7 +88,7 @@ class diff_pool_net1(torch.nn.Module):
         x1_out_up = torch.matmul(s1, x1_out) # unpool level 1
         
         # output level 0 
-        x0_out = self.gcn0_out(torch.cat((x0_, x1_out_up), 1), edge_index0)
+        x0_out = self.gcn0_out(torch.cat((x0_, x1_out_up), 1), edge_index0, edge_weight0)
     
         edge_loss = l1 + e1 +l2 + e2
         
--- experiments/varying_training_percentage/old_models/gunet_sagpool.py
+++ experiments/varying_training_percentage/old_models/gunet_sagpool.py
@@ -33,7 +33,6 @@ class SAGPool(torch.nn.Module):
     """
     def __init__(self,in_channels,ratio=0.8,Conv=GCNConv,non_linearity=torch.tanh):
         super(SAGPool,self).__init__()
-
         self.in_channels = in_channels
         self.ratio = ratio
         self.score_layer = Conv(in_channels,1)
@@ -165,18 +164,16 @@ class GraphUNet(torch.nn.Module):
 
 
 class sagpool(torch.nn.Module):
-    def __init__(self, dataset, depth, pool_ratios, dropout_rate, p_dropout_adj=0.2, p_dropout_nodes=0.92, hidden_channels=32):
+    def __init__(self, dataset, depth, pool_ratios, dropout_rate, hidden_channels=32):
         super(sagpool, self).__init__()
         self.unet = GraphUNet(in_channels=dataset.num_features, hidden_channels=hidden_channels, out_channels=dataset.num_classes, \
          depth=depth, pool_ratios=pool_ratios, dropout_rate=dropout_rate)
-        self.initial_dropout_adj = p_dropout_adj
-        self.initial_dropout_nodes = p_dropout_nodes
 
     def forward(self, data):
         edge_index, _ = dropout_adj(
-            data.edge_index, p=self.initial_dropout_adj, force_undirected=True,
+            data.edge_index, p=0.2, force_undirected=True,
             num_nodes=data.num_nodes, training=self.training)
-        x = F.dropout(data.x, p=self.initial_dropout_nodes, training=self.training)
+        x = F.dropout(data.x, p=0.92, training=self.training)
 
         x = self.unet(x, edge_index)
         return F.log_softmax(x, dim=1)
