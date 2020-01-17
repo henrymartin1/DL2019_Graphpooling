ec81be0c824321124e164c8b5c5650a5a2dd35f6
geolil <44433775+geolil@users.noreply.github.com>
2020-01-17 21:31:12 +0100

    for 3 datasets compute mean/std of best test_acc

--- experiments/datasets/datasets_run.py
+++ experiments/datasets/datasets_run.py
@@ -22,12 +22,10 @@ import numpy as np
 import seaborn as sns
 
 import torch
-from torch_geometric.datasets import Planetoid
+from torch_geometric.datasets import Planetoid, CoraFull
 from torch_geometric.utils import to_dense_adj
 import torch_geometric.transforms as T
 import torch.nn.functional as F
-
-from torch_geometric.datasets import CoraFull
 
 from networkx import from_numpy_matrix
 
@@ -67,82 +65,88 @@ def train(model_name):
     optimizer.step()
 
 
-
 # define device
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
-# define data
-dataset_name = 'Cora' # 'CoraFull', 'PubMed'
-path = osp.join(os.getcwd(), '..', 'data', dataset_name)
-dataset = Planetoid(path, dataset_name, T.NormalizeFeatures())
-data = dataset[0]
-data.batch = None
-data.adj = to_dense_adj(data.edge_index)
-
-
-
 loss_lambda = config['loss_lambda']
-
-# data 
-data = data.to(device)
-
 lr = config['optimizer']['lr']
 
 
 # define experiment
-time_tracking_dict = {'n_epochs': [],
-                      'method': [],
-                      'accuracy': []}
+acc_tracking_dict = {'dataset': [],
+                    'n_epochs': [],
+                     'method': [],
+                     'accuracy': [],
+                     'mean': [],
+                     'std_dev': []}
 
-
-t_start_all = time.time()
-for n_repeats in range(config['n_repeats']):
+for dataset_name in ['Cora', 'PubMed', 'CoraFull']:
+    # define data
+    #dataset_name = 'Cora' # 'PubMed', 'CoraFull'
+    path = osp.join(os.getcwd(), '..', 'data', dataset_name)
     
-   
-    for model_this in config['model'].keys():
-        torch.cuda.reset_max_memory_allocated(device=device)
-        model_constructor = config['model'][model_this]['model_constructor']
-        model = model_constructor(dataset, **config['model'][model_this]['parameters']).to(device)
-        optimizer = torch.optim.Adam(model.parameters(), lr=lr)
+    if (dataset_name == 'CoraFull'):
+        dataset = CoraFull(path, T.NormalizeFeatures())
+    else:
+        dataset = Planetoid(path, dataset_name, T.NormalizeFeatures())
         
-        #t_start = time.time()
-        best_val_acc = test_acc = 0
-        for epoch in range(config['epochs']):
-                
-            train(model_this)
+    data = dataset[0]
+    data.batch = None
+    data.adj = to_dense_adj(data.edge_index)
+    # data 
+    data = data.to(device)
+    
+    
+    for model_this in config['model'].keys():
+        
+        model_accuracies = []
+        
+        for n_repeats in range(config['n_repeats']):
+        
+            model_constructor = config['model'][model_this]['model_constructor']
+            model = model_constructor(dataset, **config['model'][model_this]['parameters']).to(device)
+            optimizer = torch.optim.Adam(model.parameters(), lr=lr)
+
+            best_val_acc = test_acc = 0
             
-            if epoch in config['tracked_epochs']:
+            for epoch in range(config['epochs']):
+
+                train(model_this)
+
+                #if epoch in config['tracked_epochs']:
                 accs = test(model_this)
-                t_this = time.time()-t_start
-                
+
                 train_acc, val_acc, tmp_test_acc = accs
-                
+
                 if val_acc > best_val_acc:
                     best_val_acc = val_acc
                     test_acc = tmp_test_acc
                 
-                time_tracking_dict['n_epochs'].append(epoch)
-                time_tracking_dict['time'].append(t_this)
-                time_tracking_dict['method'].append(model_this)
-                time_tracking_dict['accuracy'].append(test_acc)
+                model_accuracies.append(test_acc)
                 
-                log = 'Method: {}, Epoch: {:03d}, Train: {:.4f}, Val: {:.4f}, Test: {:.4f}, time: {:.4f}s'
-                print(log.format(model_this, epoch, train_acc, best_val_acc, test_acc, t_this))
-        max_mem = torch.cuda.max_memory_allocated(device)
-        
-        memory_tracking_dict['method'].append(model_this)
-        memory_tracking_dict['memory'].append(max_mem)
-        
-        print(max_mem)
+                acc_tracking_dict['dataset'].append(dataset_name)
+                acc_tracking_dict['n_epochs'].append(epoch)
+                acc_tracking_dict['method'].append(model_this)
+                acc_tracking_dict['accuracy'].append(test_acc)
                 
+                #mean and std of the best test_acc's so far
+                acc_tracking_dict['mean'].append(np.mean(model_accuracies)) 
+                acc_tracking_dict['std_dev'].append(np.std(model_accuracies))
+                
+                
+                if epoch in config['tracked_epochs']:
+                    log = 'Method: {}, Run: {:03d}, Epoch: {:03d}, Train: {:.4f}, Val: {:.4f}, Test: {:.4f}'
+                    print(log.format(model_this, n_repeats, epoch, train_acc, best_val_acc, test_acc))                   
+                    
+                
+             
+            
+print('finished')
 
-"""
-print('finished', 'total time', time.time()-t_start_all)
+acc_tracking_df = pd.DataFrame(data=acc_tracking_dict)
 
-time_tracking_df = pd.DataFrame(data=time_tracking_dict)
-memory_tracking_df = pd.DataFrame(data=memory_tracking_dict)
+#print(acc_tracking_df)
 
-output_dict = {'time': time_tracking_df, 'memory': memory_tracking_df}
+output_dict = {'accuracy': acc_tracking_df}
 print('Write to:',  config['output_file'])
 pickle.dump(output_dict, open( config['output_file'], "wb" ) )
-"""
